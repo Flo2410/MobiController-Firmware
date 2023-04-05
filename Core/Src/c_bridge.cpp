305d3cbf23502a6d2ee84fea937d53b1649e50b9
Flo2410 <florian@hye.dev>
2023-04-05 15:14:48 +0200

    fixed c_bridge

--- Core/Src/c_bridge.cpp
+++ Core/Src/c_bridge.cpp
@@ -7,7 +7,7 @@
 // make the method callable from C
 extern "C" {
 void mobictl_handle_commands(uint8_t min_id, uint8_t const *min_payload, uint8_t len_payload) {
-  MobiController::mobictl().handle_commands(min_id, min_payload, len_payload);
+  MobiController::mobictl().queue_command(min_id, min_payload, len_payload);
 }
 }
 #endif
