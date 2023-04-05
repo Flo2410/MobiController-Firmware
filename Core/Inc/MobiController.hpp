e25289df9b3f52ad836dbc2f8eb9c506f29606e2
Flo2410 <florian@hye.dev>
2023-04-05 13:57:50 +0200

    MobiController: made it a singleton and handle temp

--- Core/Inc/MobiController.hpp
+++ Core/Inc/MobiController.hpp
@@ -1,18 +1,60 @@
 #ifndef MOBI_CONTROLLER_H_
 #define MOBI_CONTROLLER_H_
 
+#include "Bno055.hpp"
+#include "etl/queue.h"
 #include "min.h"
 
 class MobiController {
  public:
-  MobiController(min_context *min_ctx);
+  static MobiController &mobictl() {
+    static MobiController instance;
+    return instance;
+  }
+
+  static void handle_commands(uint8_t min_id, uint8_t const *min_payload, uint8_t len_payload);
+
+  // commands
+  enum class COMMANDS {
+    IMU = 0x20,
+    ULTRASONIC_SENSOR = 0x21,
+    ENCODER = 0x22,
+    BRIGHTNESS = 0x23,
+    TEMPERATURE = 0x24,
+    BAT_VOLTAGE = 0x25,
+    USER_BUTTON = 0x26,
+    LED_STRIP = 0x27,
+    MOTOR_CONTROL = 0x28,
+    POZYX_POWER = 0x29,
+    POZYX = 0x2a,
+    POZYX_CONFIG = 0x2b,
+  };
+
+  // data
+  enum class DATA {
+    COMMAND_STAUTS = 0x0,
+    IMU = 0x1,
+    ULTRASONIC_SENSOR = 0x2,
+    ENCODER = 0x3,
+    BRIGHTNESS = 0x4,
+    TEMPERATURE = 0x5,
+    BAT_VOLTAGE = 0x6,
+    USER_BUTTON = 0x7,
+    POZYX = 0x8,
+    POZYX_CONFIG = 0x9,
+  };
+
+  Bno055 *imu;
 
   void loop();
+  void queue_data_frame(DATA data);
 
  private:
-  min_context *min_ctx;
+  MobiController();                                            // private constructor
+  MobiController(const MobiController &) = delete;             // disable copy constructor
+  MobiController &operator=(const MobiController &) = delete;  // disable copy assignment operator
 
-  ~MobiController();
+  etl::queue<DATA, 100> data_frame_queue;
 };
 
 #endif
--- Core/Src/MobiController.cpp
+++ Core/Src/MobiController.cpp
@@ -1,11 +1,50 @@
 #include "MobiController.hpp"
 
+#include "Bno055.hpp"
+#include "cpp_main.hpp"
+#include "i2c.h"
 #include "min.h"
+#include "usb_com_port.hpp"
 
-MobiController::MobiController(min_context *min_ctx) : min_ctx(min_ctx) {
-  min_init_context(this->min_ctx, 0);
+void MobiController::handle_commands(uint8_t min_id, uint8_t const *min_payload, uint8_t len_payload) {
+  switch (static_cast<COMMANDS>(min_id)) {
+    case COMMANDS::TEMPERATURE:
+      mobictl().queue_data_frame(MobiController::DATA::TEMPERATURE);
+      break;
+
+    default:
+      printf("MIN recived frame with unknown min_id!\n");
+      break;
+  }
 }
 
 void MobiController::loop() {
-  min_poll(this->min_ctx, {}, 0);
+  min_poll(&min_ctx, {}, 0);
+
+  while (this->data_frame_queue.size() > 0) {
+    DATA data;
+    this->data_frame_queue.pop_into(data);
+
+#ifdef MOBI_DEBUG
+    printf("Got item from data queue with ID: %d\n", static_cast<uint8_t>(data));
+#endif
+
+    switch (data) {
+      case DATA::TEMPERATURE:
+        USB_COM_PORT::queue_byte(DATA::TEMPERATURE, this->imu->get_temp());
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
+void MobiController::queue_data_frame(DATA data) {
+  this->data_frame_queue.push(data);
+}
+
+MobiController::MobiController() {
+  min_init_context(&min_ctx, 0);
+  this->imu = new Bno055(&hi2c1);
 }
