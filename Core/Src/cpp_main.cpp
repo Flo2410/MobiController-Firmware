dcf039ca8506d4def7373a65c70f6aa8566268cf
Flo2410 <florian@hye.dev>
2023-04-25 14:46:05 +0200

    cpp_main: printing time compilation.

--- Core/Src/cpp_main.cpp
+++ Core/Src/cpp_main.cpp
@@ -10,12 +10,13 @@ uint8_t rx_buffer[265];
 struct min_context min_ctx;
 
 int cpp_main() {
-  printf("\n----------------------------------------\n");
+  printf("\n---------------------------------------------\n");
   printf("DEBUG INTERFACE!\n");
   printf("MobiController Firmware\n");
   printf("version 0.1\n");
+  printf("Time of compilation: %s %s\n", __DATE__, __TIME__);
   printf("Copyright FHWN Florian Hye\n");
-  printf("----------------------------------------\n\n");
+  printf("---------------------------------------------\n\n");
 
   while (1) {
     MobiController::mobictl().loop();
