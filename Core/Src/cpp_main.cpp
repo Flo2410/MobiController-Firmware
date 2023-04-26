c0ee1154c785f363d05d47b85a50d5a45bd70fd9
Flo2410 <florian@hye.dev>
2023-04-26 10:07:17 +0200

    MobiCtl: removed setup methode

--- Core/Src/cpp_main.cpp
+++ Core/Src/cpp_main.cpp
@@ -18,8 +18,6 @@ int cpp_main() {
   printf("Copyright FHWN Florian Hye\n");
   printf("---------------------------------------------\n\n");
 
-  MobiController::mobictl().setup();
-
   while (1) {
     MobiController::mobictl().loop();
   }
