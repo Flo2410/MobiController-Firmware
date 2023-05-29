c88958f335f033780c7163516cfc86229a05af4b
Flo2410 <florian@hye.dev>
2023-05-29 15:07:49 +0200

    CAN_Lib: Clamp vx, vy, vphi to -3276 to 3276.
    That is done to allow it to be multiplied by 10
    and still fit into an int16.

--- Core/Src/can_lib.cpp
+++ Core/Src/can_lib.cpp
@@ -2,6 +2,7 @@
 
 #include "can.h"
 #include "stm32l4xx.h"
+#include "etl/algorithm.h"
 
 CAN_LIB::CAN_LIB(CAN_HandleTypeDef *can) {
   this->can = can;
@@ -40,6 +41,10 @@ HAL_StatusTypeDef CAN_LIB::send_stop() {
  * @retval HAL status
  */
 HAL_StatusTypeDef CAN_LIB::drive(int16_t vx, int16_t vy, int16_t vphi) {
+  vx = etl::clamp<int16_t>(vx, -3276, 3276);
+  vy = etl::clamp<int16_t>(vx, -3276, 3276);
+  vphi = etl::clamp<int16_t>(vx, -3276, 3276);
+
   int16_t val_x = vx * 10;  // m/s * 10000 -> mm/s * 10
   uint16_t x_twos = twos_complement(val_x);
   uint8_t x_high = x_twos >> 8;
